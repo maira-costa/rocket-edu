36cdbae064b05331b010839573d7d33d06152bb8
Maíra Costa <mairaccst@gmail.com>
2022-01-06 11:51:10 -0300

    otimização menu header

--- assets/js/header.js
+++ assets/js/header.js
@@ -18,3 +18,22 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+/* Menu dropdown com referência no tutorial do video localizado em https://www.youtube.com/watch?v=S-VeYcOCFZw&t=388s*/
+
+document.addEventListener("click", e => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+  if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest('[data-dropdown]')
+    currentDropdown.classList.toggle('active')
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove('active')
+  })
+})
