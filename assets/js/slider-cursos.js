72906e20e8782b63e716e36580ce869ab9916dfd
Maíra Costa <mairaccst@gmail.com>
2022-01-21 23:44:05 -0300

    Revert "ajustes em acessibilidade/videos palestras/slider"
    This reverts commit 5005f347f814839313cf75bca46f4a89709ad490.

--- assets/js/slider-cursos.js
+++ assets/js/slider-cursos.js
@@ -1,4 +1,15 @@
 
+/*
+document.querySelector("#items")
+.addEventListener("wheel", event => {
+  if(event.deltaY > 0) {
+    event.target.scrollBy(300,0)     
+  } else {
+    event.target.scrollBy(-300,0)    
+  }
+})
+
+*/
 
 new Glide(".cursos",{
   type:'carousel',
@@ -6,7 +17,9 @@ new Glide(".cursos",{
   perView: 4,  
   gap: 40,
   autoplay: 4000,
-  hoverpause:true,  
+  hoverpause:true,
+  // peek: 120,
+  
   breakpoints:{
     960:{
       perView: 3
@@ -15,7 +28,9 @@ new Glide(".cursos",{
     500:{
       perView: 1.3
     }
-  }  
+  }
+  
+  
 }).mount();
 
 
